ca8fb1c3f9206a8326c04c9bbd113271c907accb
asundar0128 <106123160+asundar0128@users.noreply.github.com>
2025-03-31 19:14:56 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,20 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='user',
     maintainer_email='user@example.com',
     description='Camera Publisher Node',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'camera_publisher = camera_publisher.camera_publisher_node:main'
+            'camera_publisher = camera_publisher.camera_publisher_node:main',
         ],
     },
 )
